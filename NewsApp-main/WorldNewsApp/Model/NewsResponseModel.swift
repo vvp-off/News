a2ecf39f8d576d6552cebf1514c7fb398b0c733d
Bokhulenkov <bokhulenkov@gmail.com>
2024-10-25 23:10:39 +0300

    build [bokhulenkov] create network service + cashe image

---
+++ NewsApp-main/WorldNewsApp/Model/NewsResponseModel.swift
@@ -0,0 +1,29 @@
+//
+//  NewsResponseModel.swift
+//  WorldNewsApp
+//
+//  Created by Alexander Bokhulenkov on 25.10.2024.
+//
+
+import Foundation
+
+// MARK: - News
+struct NewsResponseModel: Codable {
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source?
+    let author, title, description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: Date?
+    let content: String?
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String?
+}
--- NewsApp-main/WorldNewsApp/ViewController.swift
+++ NewsApp-main/WorldNewsApp/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    var articles: [News] = []
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemCyan
+        
+        //        пример поиска. категории меняются по ключу
+        fetchNews(apiService: .business)
     }
-
-
+    
+    // MARK: - Methods
+    
+    func fetchNews(apiService: ApiService) {
+        let httpClient = HTTPClient(with: .default)
+        
+        Task {
+            do {
+                let articles = try await httpClient.requestData(for: apiService)
+                self.articles = articles.map {News(from: $0) }
+                
+                // Здесь обновляем UI с нашими данными.
+                //                DispatchQueue.main.async {
+                //                self.loadImage(for: self.articles[0])
+                //                }
+                
+                //                просто тест вывода информации можно удалить
+                for sourse in articles {
+                    print(sourse.title ?? "")
+                }
+            }
+            catch let error as RequestError {
+                print("Произошла ошибка: \(error.errorDescription ?? "Неизвестная ошибка")")
+            }
+        }
+    }
+    
+    //    кеширование изображения
+    /*
+     func loadImage(for article: News) {
+     let urlToImage = article.urlToImage
+     
+     Task {
+     do {
+     if let image = try await ImageService.downloadImage(by: urlToImage) {
+     DispatchQueue.main.async {
+     //                            self.imageView.image = image
+     }
+     }
+     } catch {
+     print("Ошибка сохранения в cache: \(error.localizedDescription)")
+     }
+     }
+     }
+     */
 }
-
