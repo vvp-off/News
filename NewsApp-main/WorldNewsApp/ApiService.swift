bf14230c3d9f12a2a4dc32503b7b02e445fcdcd4
vvp-off <reservvp@gmail.com>
2024-10-26 19:24:52 +0300

    Revert "Merge remote-tracking branch 'refs/remotes/origin/bokhulenkov/network'"
    This reverts commit 54597162d911ab3952785ca4c3d35a150d9ffcdf, reversing
    changes made to a17739a6ee6e919625b665a7410a9cb1cfff4f7f.
    
    # Conflicts:
    #	NewsApp-main/.DS_Store

--- NewsApp-main/WorldNewsApp/ApiService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ApiService.swift
-//  WorldNewsApp
-//
-//  Created by Alexander Bokhulenkov on 25.10.2024.
-//
-
-import Foundation
--- NewsApp-main/WorldNewsApp/Model/ImageService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ImageService.swift
-//  WorldNewsApp
-//
-//  Created by Alexander Bokhulenkov on 25.10.2024.
-//
-
-import Foundation
