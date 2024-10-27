a26c3805b0d5cae52e23dabfee9a2ccfd18cda3b
Oksana ogay <example@email.com>
2024-10-27 10:59:40 +0500

    Language and Terms

---
+++ NewsApp-main/WorldNewsApp/Controllers/TermsConditionsViewController.swift
@@ -0,0 +1,93 @@
+//
+//  TermsConditionsViewController.swift
+//  WorldNewsApp
+//
+//
+
+import UIKit
+
+class TermsConditionsViewController: UIViewController {
+    
+    //MARK: - UI Elements
+    private lazy var navigationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Terms & Conditions"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.alwaysBounceVertical = true
+        scroll.showsVerticalScrollIndicator = true
+        scroll.backgroundColor = .white
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
+    
+    private lazy var contentView: UIView = {
+        let element = UIView()
+        element.backgroundColor = .white
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var textLabel: UILabel  = {
+        let element = UILabel()
+        element.font = UIFont.Inter.regular.size(of: 19)
+        element.textColor = AppColors.grayPrimary
+        element.lineBreakMode = .byWordWrapping
+        element.numberOfLines = 0
+        element.text = """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+        
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+        """
+        
+        element.textColor = UIColor(red: 0.49, green: 0.51, blue: 0.53, alpha: 1)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.titleView = navigationLabel
+        setupViews()
+        setupConstraints()
+    }
+    
+    //MARK: - Private Methods
+    private func setupViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(textLabel)
+    }
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            // Ограничения для UIScrollView
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            // Ограничения для contentView
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            // Ограничения для textLabel
+            textLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 120),
+            textLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            textLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -19),
+            textLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -21),
+        ])
+    }
+}
+
