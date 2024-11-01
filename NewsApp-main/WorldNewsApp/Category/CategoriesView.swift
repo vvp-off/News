2bcea7b5184ab6fcaec43828223e92e2683884aa
Dmitry <mrsahadov@gmail.com>
2024-10-31 21:36:59 -0300

    Refactor - add CategoriesViewController

--- NewsApp-main/WorldNewsApp/Category/CategoriesView.swift
+++ NewsApp-main/WorldNewsApp/Category/CategoriesView.swift
@@ -11,9 +11,6 @@ import UIKit
 class CategoriesView: UIView {
     
     //MARK: - Properties
-//     let categoryArray = ["Sports", "Politics", "Life", "Gaming", "Animals", "Nature", "Food", "Art", "History", "Fashion", "Covid-19", "Middle East"]
-    
-    let categoryArray = [{Category(name: .business)}, {Category(name: .entertainment)}, {Category(name: .general)}, {Category(name: .health)}, {Category(name: .technology)}, {Category(name: .science)}, {Category(name: .sports)}]
     
     let storageManager = StorageManager()
     var collectionView: UICollectionView!
@@ -24,7 +21,7 @@ class CategoriesView: UIView {
         title.numberOfLines = 0
         title.textAlignment = .left
         title.textColor = AppColors.blackDark
-        title.font = UIFont.Inter.semibold.size(of: 26)
+        title.font = UIFont.Inter.semibold.size(of: 24)
         title.text = "Categories"
         return title
     }()
@@ -32,12 +29,24 @@ class CategoriesView: UIView {
     let subheaderLabel: UILabel = {
         let title = UILabel()
         title.translatesAutoresizingMaskIntoConstraints = false
-        title.numberOfLines = 2
+        title.numberOfLines = 0
         title.textAlignment = .left
         title.textColor = AppColors.grayPrimary
         title.font = UIFont.Inter.regular.size(of: 18)
         title.text = "Thousands of articles in each category"
         return title
+    }()
+    
+    let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Next", for: .normal)
+        button.titleLabel?.font = UIFont.Inter.bold.size(of: 18)
+        button.backgroundColor = AppColors.purplePrimary
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isUserInteractionEnabled = true
+        button.isHidden = true
+        return button
     }()
     
     
@@ -56,7 +65,7 @@ class CategoriesView: UIView {
     //MARK: - Setup UI
     private func setupViews() {
         backgroundColor = .white
-        [headerLabel, subheaderLabel].forEach { addSubview($0) }
+        [headerLabel, subheaderLabel, button].forEach { addSubview($0) }
     }
     
 }
@@ -69,9 +78,6 @@ private extension CategoriesView {
         layout.minimumLineSpacing = 20
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: "cell")
         addSubview(collectionView)
     }
 }
@@ -83,76 +89,17 @@ private extension CategoriesView {
             headerLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
             subheaderLabel.topAnchor.constraint(equalTo: headerLabel.topAnchor, constant: 40),
             subheaderLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            collectionView.topAnchor.constraint(equalTo: subheaderLabel.topAnchor, constant: 50),
-            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50),
+            subheaderLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            collectionView.topAnchor.constraint(equalTo: subheaderLabel.bottomAnchor, constant: 30),
+            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -200),
             collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20)
+            collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -150),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            button.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
-
-
-extension CategoriesView: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categoryArray.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell {
-            
-            
-            let categories = storageManager.getCategories(forKey: .categories)
-            guard let category = cell.category else { return }
-            
-            if !categories.contains(where: { $0.name == category.name }) {
-                cell.backgroundColor = AppColors.purplePrimary
-                cell.titleLabel.textColor = .white
-                storageManager.addGategory(category, forKey: .categories)
-            } else {
-                //cell.backgroundColor = AppColors.lightBlue
-                cell.backgroundColor = .white
-                cell.titleLabel.textColor = AppColors.grayPrimary
-                
-                storageManager.removeCategory(category, forKey: .categories)
-            }
-        }
-    }
-    
-}
-
-extension CategoriesView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CategoryCell
-        cell.configure(with: categoryArray[indexPath.row]())
-        
-        
-        let categories = storageManager.getCategories(forKey: .categories)
-        let category = cell.category
-        
-        if categories.contains(where: { $0.name == category!.name }) {
-            cell.backgroundColor = AppColors.purplePrimary
-            cell.titleLabel.textColor = .white
-        } else {
-            cell.backgroundColor = .white
-            //cell.backgroundColor = AppColors.lightBlue
-            cell.titleLabel.textColor = AppColors.grayPrimary
-        }
-        
-        return cell
-    }
-}
-
-extension CategoriesView: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let padding: CGFloat = 20 // Padding for the collection view edges and item spacing
-        let itemWidth = (collectionView.frame.width - padding) / 2 // Two cells per row
-        return CGSize(width: itemWidth, height: 72)
-    }
-    
-    
-}
-
-
-
 
 
--- NewsApp-main/WorldNewsApp/Category/CategoriesViewController.swift
+++ NewsApp-main/WorldNewsApp/Category/CategoriesViewController.swift
@@ -14,20 +14,110 @@ class CategoriesViewController: UIViewController {
     let storageManager = StorageManager()
     
     var categories = [Category]()
+    let categoryArray = [{Category(name: .business)}, {Category(name: .entertainment)}, {Category(name: .general)}, {Category(name: .health)}, {Category(name: .technology)}, {Category(name: .science)}, {Category(name: .sports)}]
+    
+    var cellColor = UIColor(named: "white")
+    let onBoardingIsDone = true
 
     //MARK: - Life cycle
     override func loadView() {
         view = categoriesView
+        categoriesView.collectionView.dataSource = self
+        categoriesView.collectionView.delegate = self
+        categoriesView.collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: "cell")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         categories = storageManager.getCategories(forKey: .categories)
-        print(categories.count)
+        
+        if !onBoardingIsDone {
+            setCategoryBoarding()
+        }
+        
+        categoriesView.button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
     }
 
     //MARK: - Methods
 
 }
+
+extension CategoriesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell {
+            
+            
+            let categories = storageManager.getCategories(forKey: .categories)
+            guard let category = cell.category else { return }
+            
+            if !categories.contains(where: { $0.name == category.name }) {
+                cell.backgroundColor = AppColors.purplePrimary
+                cell.titleLabel.textColor = .white
+                storageManager.addGategory(category, forKey: .categories)
+            } else {
+                //cell.backgroundColor = AppColors.lightBlue
+                cell.backgroundColor = cellColor
+                cell.titleLabel.textColor = AppColors.grayPrimary
+                
+                storageManager.removeCategory(category, forKey: .categories)
+            }
+        }
+    }
+    
+}
+
+extension CategoriesViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CategoryCell
+        cell.configure(with: categoryArray[indexPath.row]())
+        
+        
+        let categories = storageManager.getCategories(forKey: .categories)
+        let category = cell.category
+        
+        if categories.contains(where: { $0.name == category!.name }) {
+            cell.backgroundColor = AppColors.purplePrimary
+            cell.titleLabel.textColor = .white
+        } else {
+            cell.backgroundColor = cellColor
+            //cell.backgroundColor = AppColors.lightBlue
+            cell.titleLabel.textColor = AppColors.grayPrimary
+        }
+        
+        return cell
+    }
+}
+
+extension CategoriesViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 20
+        let itemWidth = (collectionView.frame.width - padding) / 2
+        return CGSize(width: itemWidth, height: 72)
+    }
+    
+    
+}
+
+extension CategoriesViewController {
+    func setCategoryBoarding(){
+        categoriesView.headerLabel.text = "Select your favourite topics"
+        categoriesView.subheaderLabel.text = "Select some of your favourite topics to let us suggest better news for you"
+        cellColor = AppColors.lightBlue
+        categoriesView.button.isHidden = false
+    }
+    
+    @objc func buttonTapped(){
+        tabBarController?.selectedIndex = 0
+    }
+}
+
+
+
+
+
 
