28427e0809dacec2a25b60c41292351e27ffda14
Igor Guryan <sacujo93@yandex.ru>
2024-11-03 00:22:54 +0300

    hotfix

--- NewsApp-main/WorldNewsApp/SceneDelegate.swift
+++ NewsApp-main/WorldNewsApp/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        window?.rootViewController = ViewController()
 //        window?.makeKeyAndVisible()
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ResultViewController()
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController()) 
         window?.makeKeyAndVisible()
     }
 }
