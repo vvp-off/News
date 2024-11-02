2f226fc31c19aa0bee4b3c2898f83581eba5862c
Bokhulenkov <bokhulenkov@gmail.com>
2024-11-03 00:22:03 +0300

    fix [bokhulenkov] translate onboarding to eng

--- NewsApp-main/WorldNewsApp/Extensions/Constants.swift
+++ NewsApp-main/WorldNewsApp/Extensions/Constants.swift
@@ -9,14 +9,14 @@ import Foundation
 
 struct K {
     enum titleNews: String {
-        case page1 = "Будьте в курсе главного"
-        case page2 = "Лента, которая вам подходит"
-        case page3 = "Ваши интересы на первом месте"
+        case page1 = "Stay on Top of What Matters"
+        case page2 = "A Feed That Fits You"
+        case page3 = "Your Interests Come First"
     }
     
-    static let page1Description = "Получайте доступ к самым популярным новостям в мире. Узнавайте о важных событиях, трендах и актуальных темах первым. Оставайтесь всегда в центре событий!"
-    static let page2Description = "Мы собрали новости по категориям, чтобы вам было проще найти интересующее. От политики до технологий — выбирайте то, что действительно важно для вас."
-    static let page3Description = "Персонализируйте свою ленту и следите только за теми темами, которые вам важны. Читайте больше о том, что вам интересно, и тратьте время с пользой!"
+    static let page1Description = "Get instant access to the world’s most popular news. Be the first to know about important events, trends, and the latest topics. Always stay in the know!"
+    static let page2Description = "We’ve organized news by category to make finding what you’re interested in easier. From politics to technology—choose what really matters to you."
+    static let page3Description = "Personalize your feed and follow only the topics that are important to you. Spend more time on what you care about and stay informed efficiently!"
     static let page1Image = "page1"
     static let page2Image = "page2"
     static let page3Image = "page3"
